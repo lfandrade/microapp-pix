fe8604240318721be48bcdae90ae47f593c695fa
Andrade <luisfelipeandrade@gmail.com>
2024-10-14 17:16:33 -0300

    Version 01

---
+++ lib/pix_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PixScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pix'),
+      ),
+      body: Center(
+        child: Text('Tela do Pix'),
+      ),
+    );
+  }
+}
